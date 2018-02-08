9bd06392844ca63547e592889af83fab26aeb818
Alejandro Alvarez <alejandro.alvarez@technifiser.com>
2018-02-08 14:39:34 -0400

    listo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hola!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import './style.scss';
+
+ReactDOM.render(<App hola="siii" d/>,
+  document.getElementById('root')
+)
